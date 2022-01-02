368d4e9165f4741a7185125759cc0d8053084d05
Pedro Lima <pedrogglima@gmail.com>
2022-01-02 17:20:49 -0300

    fix seed post

--- db/seeds/2_posts.rb
+++ db/seeds/2_posts.rb
@@ -4,11 +4,7 @@ posts_per_user = rand(10..15)
 
 User.all.each do |user|
   posts_per_user.times do
-    Post.create!(
-      user: user,
-      postable: PostType::Simple.create!(
-        content: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
-      )
-    )
+    resource_params = { content: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false) }
+    PostType::Simple.build_with_post(resource_params, user).save!
   end
 end
