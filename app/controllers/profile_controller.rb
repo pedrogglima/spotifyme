e5bc159ba8afffadf53839de201e3ed69193cd06
Pedro Lima <pedrogglima@gmail.com>
2022-01-04 21:03:53 -0300

    add infinite_scroll to profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -7,5 +7,15 @@ class ProfileController < PrivateApplicationController
       current_user.simple_posts.order('posts.created_at': :desc),
       items: 10
     )
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: {
+          entries: render_to_string(partial: 'post_type/simples/posts', formats: [:html]),
+          pagination: view_context.pagy_nav(@pagy)
+        }
+      end
+    end
   end
 end
