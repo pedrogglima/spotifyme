1394e4b559a53539489c14037f6d2183ba110cc2
Pedro Lima <pedrogglima@gmail.com>
2022-01-08 15:49:57 -0300

    fix feeds (only current user access)

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,19 +1,7 @@
 # frozen_string_literal: true
 
 class FeedsController < PrivateApplicationController
-  before_action :set_current_user_feed
-
   def index
-    @pagy, @feeds = pagy(Feed.by_user(params[:user_id]))
-  end
-
-  private
-
-  def set_current_user_feed
-    @current_user_feed = if params[:user_id]
-                           User.find(params[:user_id])
-                         else
-                           current_user
-                         end
+    @pagy, @feeds = pagy(Feed.by_user(current_user.id))
   end
 end
