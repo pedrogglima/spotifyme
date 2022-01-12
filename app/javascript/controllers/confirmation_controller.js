1b59ed127b2138943fb101ca26fd2a298a392dc7
Pedro Lima <pedrogglima@gmail.com>
2022-01-11 23:22:02 -0300

    lint stimulus controllers

--- app/javascript/controllers/confirmation_controller.js
+++ app/javascript/controllers/confirmation_controller.js
@@ -1,11 +1,11 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static values = { message: String };
 
   confirm(event) {
-    if (!(window.confirm(this.messageValue))) {
-      event.preventDefault()
-    };
-  };
+    if (!window.confirm(this.messageValue)) {
+      event.preventDefault();
+    }
+  }
 }
--- app/javascript/controllers/infinite_scroll_controller.js
+++ app/javascript/controllers/infinite_scroll_controller.js
@@ -1,46 +1,52 @@
-import { Controller } from '@hotwired/stimulus'
-import { FetchRequest } from '@rails/request.js'
+import { Controller } from "@hotwired/stimulus";
+import { FetchRequest } from "@rails/request.js";
 
 export default class extends Controller {
-  static targets = ['entries', 'pagination']
-  static values = { fetching: Boolean }  
-  
+  static targets = ["entries", "pagination"];
+  static values = { fetching: Boolean };
+
   async scroll() {
-    const next_page = this.paginationTarget.querySelector("a[rel='next']")
-    if (next_page == null) { return }
+    const next_page = this.paginationTarget.querySelector("a[rel='next']");
+    if (next_page == null) {
+      return;
+    }
 
-    let url = next_page.href
-    let body = document.body, html = document.documentElement, bottomTrashold = 250;
+    let url = next_page.href;
+    let body = document.body,
+      html = document.documentElement,
+      bottomTrashold = 250;
 
-    var height = Math.max( body.scrollHeight, 
-      body.offsetHeight, 
-      html.clientHeight, 
-      html.scrollHeight, 
-      html.offsetHeight 
+    var height = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
     );
 
-    const reachBottom = window.pageYOffset >= height - window.innerHeight - bottomTrashold
-     
-    if (reachBottom && this.fetchingValue == false) {
-      this.fetchingValue = true
+    const reachBottom =
+      window.pageYOffset >= height - window.innerHeight - bottomTrashold;
 
-      await this.loadContent(url)
+    if (reachBottom && this.fetchingValue == false) {
+      this.fetchingValue = true;
+
+      await this.loadContent(url);
     }
   }
 
   async loadContent(url) {
-    const request = new FetchRequest("get", url, { responseKind: 'json' })
-    const response = await request.perform()
-    
+    const request = new FetchRequest("get", url, { responseKind: "json" });
+    const response = await request.perform();
+
     if (response.ok) {
-      const data = await response.json
-      
-      this.entriesTarget.insertAdjacentHTML('beforeend', data.entries)
-      this.paginationTarget.innerHTML = data.pagination
-      this.fetchingValue = false
+      const data = await response.json;
+
+      this.entriesTarget.insertAdjacentHTML("beforeend", data.entries);
+      this.paginationTarget.innerHTML = data.pagination;
+      this.fetchingValue = false;
     } else {
       // TODO: handle error
-      this.fetchingValue = false
+      this.fetchingValue = false;
     }
   }
 }
