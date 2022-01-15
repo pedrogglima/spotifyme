f3214fa487ce70f27c7f0cdc1f1a1f7b8ded6255
Pedro Lima <pedrogglima@gmail.com>
2022-01-15 19:49:02 -0300

    fix dependent destroy to on post / notifications

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,16 @@
 class Post < ApplicationRecord
   extend PostQuery
 
-  ALLOWED_TYPES = %w[Posts::User].freeze
-
   belongs_to :user
   belongs_to :postable, polymorphic: true, dependent: :destroy
+  has_many :like_notifications,
+           class_name: 'Notifications::OfLike',
+           dependent: :destroy
   # TODO: fix queries
   # has_many :comments, through: post, source: :commentable, source_type: 'Posts::User'
   # has_many :likes, through: post, source: :likeable, source_type: 'Posts::User'
+
+  ALLOWED_TYPES = %w[Posts::User].freeze
 
   validates :user_id, presence: true
   validates :postable, presence: true
