8ef77eb2ebb4d6ba0ba7e7ef7c5e987d4e9cae00
Pedro Lima <pedrogglima@gmail.com>
2022-01-16 14:38:29 -0300

    add notification query

--- app/queries/notification_query.rb
+++ app/queries/notification_query.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module NotificationQuery
+  def by_user(user_id)
+    select(stringify_all_types)
+      .joins("LEFT JOIN notifications_of_likes AS of_likes ON notifications.notificable_type = 'Notifications::OfLike' AND notifications.notificable_id = of_likes.id")
+      .joins("LEFT JOIN notifications_of_invites AS of_invites ON notifications.notificable_type = 'Notifications::OfInvites' AND notifications.notificable_id = of_invites.id")
+      .where(destinatary_id: user_id)
+      .order(created_at: :desc)
+  end
+
   def of_like(user_id)
     select(stringify_notification)
       .joins("LEFT JOIN notifications_of_likes AS of_likes ON notifications.notificable_type = 'Notifications::OfLike' AND notifications.notificable_id = of_likes.id")
@@ -33,6 +41,22 @@ module NotificationQuery
       of_likes.post_id as of_like_post_id,
       of_likes.content as of_like_content,
       of_likes.seen as of_like_seen
+    ".squish.freeze
+  end
+
+  def stringify_all_types
+    "
+      notifications.id,
+      notifications.destinatary_id,
+      notifications.notificable_id,
+      notifications.notificable_type,
+      of_likes.id as of_like_id,
+      of_likes.post_id as of_like_post_id,
+      of_likes.content as of_like_content,
+      of_likes.seen as of_like_seen,
+      of_invites.id as of_invite_id,
+      of_invites.content as of_invite_content,
+      of_invites.seen as of_invite_seen
     ".squish.freeze
   end
 end
