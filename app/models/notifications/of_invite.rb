d22d5a4793ab7e3de6b2933b6558da826cce3cf6
Pedro Lima <pedrogglima@gmail.com>
2022-01-20 21:04:29 -0300

    update models to broadcast & nested_attributes notifications

--- app/models/notifications/of_invite.rb
+++ app/models/notifications/of_invite.rb
@@ -3,6 +3,33 @@
 class Notifications::OfInvite < ApplicationRecord
   has_one :notification, as: :notificable, dependent: :destroy
 
+  after_create_commit do
+    broadcast_replace_to(
+      "user_#{notification.destinatary_id}:all",
+      target: 'notifications_alert',
+      partial: 'notifications/alert'
+    )
+  end
+
+  accepts_nested_attributes_for :notification
+
   validates :content, presence: true
   validates :seen, inclusion: { in: [true, false] }
+
+  # Fix for nested attr. association polymorphic
+  def notification_attributes=(attribute_set)
+    super(attribute_set.merge(notificable: self))
+  end
+
+  # Fix for nested attr. association polymorphic
+  def self.build_with_notification(params, follower_id)
+    new_params = params.to_h
+                       .deep_merge!(
+                         notification_attributes: {
+                           destinatary_id: follower_id
+                         }
+                       )
+
+    new(new_params)
+  end
 end
--- app/models/notifications/of_like.rb
+++ app/models/notifications/of_like.rb
@@ -4,8 +4,32 @@ class Notifications::OfLike < ApplicationRecord
   has_one :notification, as: :notificable, dependent: :destroy
   belongs_to :post
 
+  after_create_commit do
+    broadcast_replace_to(
+      "user_#{notification.destinatary_id}:all",
+      target: 'notifications_alert',
+      partial: 'notifications/alert'
+    )
+  end
+
   accepts_nested_attributes_for :notification
 
   validates :content, presence: true
   validates :seen, inclusion: { in: [true, false] }
+
+  # Fix for nested attr. association polymorphic
+  def notification_attributes=(attribute_set)
+    super(attribute_set.merge(notificable: self))
+  end
+
+  # Fix for nested attr. association polymorphic
+  def self.build_with_notification(params, following_id)
+    new_params = params.to_h
+                       .deep_merge!(
+                         notification_attributes: {
+                           destinatary_id: following_id
+                         }
+                       )
+    new(new_params)
+  end
 end
