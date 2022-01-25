a486018bd79d1100a963396a97d464d0a8310f2f
Pedro Lima <pedrogglima@gmail.com>
2022-01-25 11:20:54 -0300

    add spotify pallet color on tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,28 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*',
-    './app/components/**/*'
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*",
+    "./app/components/**/*",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "spotify-green": "#1ed760",
+        "spotify-green2": "#1db954",
+        "spotify-black": "#191414",
+        "spotify-white": "#fafafa",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
+};
