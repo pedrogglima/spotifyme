bed2fdbf6261ec31d3d1c6f3dc62c8428d9c0680
Pedro Lima <pedrogglima@gmail.com>
2022-01-26 22:35:46 -0300

    update visited set to private application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def current_visited
-    @visited
-  end
-
-  def current_user_profile?
-    current_visited.nil? ? true : false
-  end
-  helper_method :current_user_profile?, :current_visited
 end
--- app/controllers/private_application_controller.rb
+++ app/controllers/private_application_controller.rb
@@ -4,4 +4,19 @@ class PrivateApplicationController < ApplicationController
   include Pagy::Backend
 
   before_action :authenticate_user!
+  before_action :set_visited_if
+
+  def set_visited_if
+    @visited = User.find(params[:user_id]) if params[:user_id].present? &&
+                                              params[:user_id].to_i != current_user.id
+  end
+
+  def current_visited
+    @visited
+  end
+
+  def current_user_profile?
+    current_visited.nil? ? true : false
+  end
+  helper_method :current_user_profile?, :current_visited
 end
