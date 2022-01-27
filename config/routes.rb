57c4ebd15494465682886264b82d68470e350a37
Pedro Lima <pedrogglima@gmail.com>
2022-01-26 22:37:25 -0300

    add sidekiq web on routes (missing auth)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   devise_for :users,
              class_name: 'User',
              controllers: {
