195e47ae3819526b34e06e174664df0bdbe94fe0
Pedro Lima <pedrogglima@gmail.com>
2022-01-26 22:33:26 -0300

    change namespace for notifications workers

--- app/workers/notifications/invite_worker.rb
+++ app/workers/notifications/invite_worker.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-class Notifications::InviteWorker < BaseWorker
-  include Sidekiq::Worker
+module Notifications
+  class InviteWorker < BaseWorker
+    include Sidekiq::Worker
 
-  def perform(follower_id, following_id)
-    args = { following_id: following_id }
+    def perform(follower_id, following_id)
+      args = { following_id: following_id }
 
-    ::Generators::InviteNotification.with(args)
-                                    .deliver_from(follower_id)
+      ::Generators::InviteNotification.with(args)
+                                      .deliver_from(follower_id)
+    end
   end
 end
--- app/workers/notifications/like_worker.rb
+++ app/workers/notifications/like_worker.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-class Notifications::LikeWorker < BaseWorker
-  include Sidekiq::Worker
+module Notifications
+  class LikeWorker < BaseWorker
+    include Sidekiq::Worker
 
-  def perform(post_id, follower_id)
-    args = { post_id: post_id }
+    def perform(post_id, follower_id)
+      args = { post_id: post_id }
 
-    ::Generators::LikeNotification.with(args)
-                                  .deliver_from(follower_id)
+      ::Generators::LikeNotification.with(args)
+                                    .deliver_from(follower_id)
+    end
   end
 end
